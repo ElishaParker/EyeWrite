fbf22e6766c8aa79dda873d2267db1e555d2dd77
ElishaParker <elishaparker@hotmail.com>
2025-10-30 21:19:53 -0400

    Update webgazer.js

--- assets/libs/webgazer.js
+++ assets/libs/webgazer.js
@@ -1,1 +1,9 @@
-// Placeholder for WebGazer.js library
+// Placeholder for WebGazer gaze tracking
+// Official repo: https://github.com/brownhci/WebGazer
+window.webgazer={
+  begin:()=>console.warn("webgazer.begin() placeholder"),
+  setGazeListener:()=>{},
+  clearGazeListener:()=>{},
+  showVideo:()=>{},
+  showPredictionPoints:()=>{}
+};
